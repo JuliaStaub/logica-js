6c9928e7c25c245889935590090a2279cad1772a
Júlia Staub <juliastaub02@gmail.com>
2025-04-14 18:01:00 -0300

    mudando o título com manipulação de DOM

---
+++ app.js
@@ -0,0 +1,15 @@
+let numeroSecreto;
+let tentativas;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 10:');
+}
+
+exibirMensagemInicial();
+
